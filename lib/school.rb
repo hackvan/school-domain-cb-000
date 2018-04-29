c4c30d93a4156425989a1da677a7d8d3a373db65
Felice Forby <feliceforby@gmail.com>
2018-04-29 23:52:48 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,8 +18,7 @@ class School
   def sort
     sorted_roster = {}
     @roster.each do |grade, students|
-      sorted_roster[grade] = []
-      sorted_roster[grade] << students.sort
+      sorted_roster[grade] = students.sort
     end
     sorted_roster
   end
